4b93cad465d31012dc59b45ab5cc3223b8706628
igor-baiborodine <igor@kiroule.com>
2024-09-17 20:15:51 -0400

    test: add campsites data generator

---
+++ datagenerator/go.mod
@@ -0,0 +1,18 @@
+module github.com/igor-baiborodine/campsite-booking-go/datagenerator
+
+go 1.22.4
+
+require (
+	github.com/go-faker/faker/v4 v4.4.1
+	github.com/igor-baiborodine/campsite-booking-go v1.0.11
+	google.golang.org/grpc v1.66.2
+)
+
+require (
+	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.33.0-20240401165935-b983156c5e99.1 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
+)
---
+++ datagenerator/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/go-faker/faker/v4"
+	api "github.com/igor-baiborodine/campsite-booking-go/campgroundspb/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) == 0 {
+		log.Fatalln("usage: datagenerator [SERVER_ADDR] [CAMPSITES_COUNT]")
+	}
+	addr := args[0]
+	campsitesCount := 100
+	if len(args) > 1 {
+		campsitesCount, _ = strconv.Atoi(args[1])
+	}
+
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect to grpc server at %s: %v", addr, err)
+	}
+	defer func(conn *grpc.ClientConn) {
+		if err := conn.Close(); err != nil {
+			log.Fatalf("failed to close connection: %v", err)
+		}
+	}(conn)
+
+	client := api.NewCampgroundsServiceClient(conn)
+	campsitesIDs := createCampsites(client, campsitesCount)
+	log.Printf("created %d campsites: %v", len(campsitesIDs), campsitesIDs)
+}
+
+func createCampsites(c api.CampgroundsServiceClient, count int) (campsiteIDs []string) {
+	for i := 0; i < count; i++ {
+		newCreateCampsiteRequest()
+		response, err := c.CreateCampsite(context.Background(), newCreateCampsiteRequest())
+		if err != nil {
+			log.Fatalf("failed to create campsite: %v", err)
+		}
+		campsiteIDs = append(campsiteIDs, response.CampsiteId)
+	}
+	return campsiteIDs
+}
+
+func newCreateCampsiteRequest() *api.CreateCampsiteRequest {
+	req := api.CreateCampsiteRequest{}
+	err := faker.FakeData(&req)
+	if err != nil {
+		log.Fatalf("failed to fake CreateCampsiteRequest: %v", err)
+		return nil
+	}
+	return &req
+}
